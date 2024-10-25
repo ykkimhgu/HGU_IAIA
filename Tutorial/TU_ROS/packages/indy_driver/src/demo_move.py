56fb0df751405931e164d19efa6fd0f4597e6abb
hyKang <91526930+hyKangHGU@users.noreply.github.com>
2024-10-25 10:47:05 +0900

    Update demo_move.py

--- Tutorial/TU_ROS/packages/indy_driver/src/demo_move.py
+++ Tutorial/TU_ROS/packages/indy_driver/src/demo_move.py
@@ -8,41 +8,37 @@ import rospy
 def main():
     try:
 
-        indy10_interface = MoveGroupPythonInterface(real=False)
+        indy10 = MoveGroupPythonInterface(real=False)
         
         print("============ Moving initial pose using a joint state goal ...")
+        # indy10.move_to_standby()
         init_pose_joints = [0, 0, tau/4, 0, tau/4, 0]          # tau = 2 * pi
-        indy10_interface.go_to_joint_abs(init_pose_joints)
+        indy10.go_to_joint_abs(init_pose_joints)
 
-        input("============ Press `Enter` to execute a movement using a joint state goal ...")
+        input("============ Press `Enter` to execute a movement using a joint state goal(relative) ...")
         joint_rel = [1/8 * tau, 0, 0, 0, 0, 0]          # tau = 2 * pi
-        indy10_interface.go_to_joint_rel(joint_rel)
+        indy10.go_to_joint_rel(joint_rel)
         
-        input("============ Press `Enter` to execute a movement using a joint state goal ...")
+        input("============ Press `Enter` to execute a movement using a joint state goal(relative) ...")
         joint_rel = [-1/8 * tau, 0, 0, 0, 0, 0]          # tau = 2 * pi
-        indy10_interface.go_to_joint_rel(joint_rel)
+        indy10.go_to_joint_rel(joint_rel)
 
         input("============ Press `Enter` to execute a movement using a relative pose ...")
         target_pose_rel_xyz = [0.0, 0.0, 0.0]
-        target_pose_rel_rpy = [0, -tau/2, 0]
-        indy10_interface.go_to_pose_rel(target_pose_rel_xyz, target_pose_rel_rpy)
+        target_pose_rel_rpy = [tau/8, 0, 0]
+        indy10.go_to_pose_rel(target_pose_rel_xyz, target_pose_rel_rpy)
+                
+        input("============ Press `Enter` to execute a movement using a relative pose ...")
+        target_pose_rel_xyz = [0.0, 0.0, 0.0]
+        target_pose_rel_rpy = [-tau/8, 0, 0]
+        indy10.go_to_pose_rel(target_pose_rel_xyz, target_pose_rel_rpy)
         
-        input("============ Press `Enter` to execute a movement using a relative pose ...")
-        target_pose_rel_xyz = [0.0, 0.0, 0.0]
-        target_pose_rel_rpy = [0, tau/2, 0]
-        indy10_interface.go_to_pose_rel(target_pose_rel_xyz, target_pose_rel_rpy)
-
         input("============ Press `Enter` to execute a movement using a absolute pose ...")
         target_pose_abs_xyz = [0.0, 0.52, 0.54]
-        target_pose_abs_rpy = [0, 1/2*tau, 0]
-        indy10_interface.go_to_pose_abs(target_pose_abs_xyz, target_pose_abs_rpy)
+        target_pose_abs_rpy = [-2.3553, 0.0003, 0.0008]
+        indy10.go_to_pose_abs(target_pose_abs_xyz, target_pose_abs_rpy)
 
-        # input("============ Press `Enter` to execute a movement using a absolute pose ...")
-        # target_pose_abs_xyz = [0.0, 0.0, 0.0]
-        # target_pose_abs_rpy = [0, 0, 0]
-        # indy10_interface.go_to_pose_abs(target_pose_abs_xyz, target_pose_abs_rpy)
-
-        print("============ Python tutorial demo complete!")
+        print("============ complete!")
 
     except rospy.ROSInterruptException:
         return
